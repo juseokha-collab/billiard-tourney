b74caef4718248ebdfff3ed558d90c46082f4620
juseokha-collab <juseok.ha@gmail.com>
2026-09-01 10:06:02 +0900

    Initial deploy: billiard tournament app with Firebase auth + Firestore sync

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// 기존 하루/인맥성좌 앱과 같은 Firebase 프로젝트(haru-d9bdd)를 재사용합니다.
+// 이 앱은 'billiard-tourney-2026' 컬렉션만 별도로 사용하므로 다른 앱 데이터와 섞이지 않습니다.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA7vy1c-yb0AmYZKpk71-o0IJB0fDNVbT4",
+  authDomain: "haru-d9bdd.firebaseapp.com",
+  projectId: "haru-d9bdd",
+  storageBucket: "haru-d9bdd.firebasestorage.app",
+  messagingSenderId: "1078904759332",
+  appId: "1:1078904759332:web:03f6a5fb74109c297ea4b1",
+  measurementId: "G-GEJRCEC0QM"
+};
